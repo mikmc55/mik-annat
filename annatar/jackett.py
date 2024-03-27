f4f4d15e88da599e47e94aa9a1ab8995c4786c11
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-03-27 07:16:02 +0000

    Update jackett.py

--- annatar/jackett.py
+++ annatar/jackett.py
@@ -24,8 +24,8 @@ from annatar.torrent import Category
 log = structlog.get_logger(__name__)
 
 
-JACKETT_URL: str = os.environ.get("JACKETT_URL", "http://195.154.231.70:9117")
-JACKETT_API_KEY: str = os.environ.get("JACKETT_API_KEY", "ar2u3emdwocp9cqcbyiugot0t17s65yl")
+JACKETT_URL: str = os.environ.get("JACKETT_URL", "http://1.14.73.37:9117")
+JACKETT_API_KEY: str = os.environ.get("JACKETT_API_KEY", "ft950g440swlqnrk8vpu35s533ok1fo8")
 
 JACKETT_MAX_RESULTS = int(os.environ.get("JACKETT_MAX_RESULTS", 100))
 JACKETT_TIMEOUT = int(os.environ.get("JACKETT_TIMEOUT", 6))
@@ -35,7 +35,7 @@ JACKETT_CACHE_MINUTES = timedelta(minutes=int(os.environ.get("JACKETT_CACHE_MINU
 
 JACKETT_INDEXERS_LIST: list[str] = os.environ.get(
     "JACKETT_INDEXERS",
-    "aither-api,avistaz,bitsearch,bwtorrents,digitalcore,filelist,greatposterwall,hawke-uno,hdtorrents,lst,morethantv-api,privatehd,rutracker,skipthetrailers,solidtorrents,torrentleech,torrentseeds-api",
+    "btsow,hdarea,hdatmos,hdfans,monikadesign-api,passthepopcorn,thesceneplace,torlock,xspeeds",
 ).split(",")
 
 
