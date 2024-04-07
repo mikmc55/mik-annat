80e5bd369ead02d6327321daf86862c9443674e0
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-04-08 00:56:36 +0100

    Update jackett.py

--- annatar/jackett.py
+++ annatar/jackett.py
@@ -25,7 +25,7 @@ log = structlog.get_logger(__name__)
 
 
 JACKETT_URL: str = os.environ.get("JACKETT_URL", "http://1.14.73.37:9117")
-JACKETT_API_KEY: str = os.environ.get("JACKETT_API_KEY", "ar2u3emdwocp9cqcbyiugot0t17s65yl")
+JACKETT_API_KEY: str = os.environ.get("JACKETT_API_KEY", "ft950g440swlqnrk8vpu35s533ok1fo8")
 
 JACKETT_MAX_RESULTS = int(os.environ.get("JACKETT_MAX_RESULTS", 100))
 JACKETT_TIMEOUT = int(os.environ.get("JACKETT_TIMEOUT", 6))
@@ -35,7 +35,7 @@ JACKETT_CACHE_MINUTES = timedelta(minutes=int(os.environ.get("JACKETT_CACHE_MINU
 
 JACKETT_INDEXERS_LIST: list[str] = os.environ.get(
     "JACKETT_INDEXERS",
-    "aither-api,avistaz,bitsearch,bwtorrents,digitalcore,filelist,greatposterwall,hawke-uno,hdtorrents,lst,morethantv-api,privatehd,rutracker,skipthetrailers,solidtorrents,torrentleech,torrentseeds-api",
+    "btsow,cinemageddon,hdarea,hdatmos,hdfans,hdsky,milkie,monikadesign-api,ncore,passthepopcorn,thesceneplace,torrentseeds-api,xspeeds",
 ).split(",")
 
 
